6edc1496ca7ea84277eb34af83aabcf288d189e6
Jason Tan <jtan20040401@gmail.com>
2023-10-27 15:03:45 +1100

    Customize HATEOAS resource path

--- src/main/java/com/bw/petclinic/vet/repository/VetRepository.java
+++ src/main/java/com/bw/petclinic/vet/repository/VetRepository.java
@@ -2,9 +2,10 @@ package com.bw.petclinic.vet.repository;
 
 import com.bw.petclinic.vet.domain.Vet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@RepositoryRestResource(path = "veterinarians")
 public interface VetRepository extends JpaRepository<Vet, Integer> {
 
 }
