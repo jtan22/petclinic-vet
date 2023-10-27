34fb92e1b5acc9aca55072e6c45cbd45b8b2af46
Jason Tan <jtan20040401@gmail.com>
2023-10-27 12:05:22 +1100

    Changed Crud and Paging Repository to just JpaRepository

--- src/main/java/com/bw/petclinic/vet/repository/VetRepository.java
+++ src/main/java/com/bw/petclinic/vet/repository/VetRepository.java
@@ -3,12 +3,11 @@ package com.bw.petclinic.vet.repository;
 import com.bw.petclinic.vet.domain.Vet;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VetRepository extends PagingAndSortingRepository<Vet, Integer>, CrudRepository<Vet, Integer> {
+public interface VetRepository extends JpaRepository<Vet, Integer> {
 
     Page<Vet> findPage(Pageable pageable);
 
